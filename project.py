40114f016fb3923e50bce8488f3a2c2e0daf30a1
Jase <jaarbr@gmail.com>
2022-12-04 20:21:25 -0600

    Finish Query 3

--- project.py
+++ project.py
@@ -5,16 +5,22 @@
 #                       K -> C)
 
 from tkinter import *
+from tkinter import ttk
+
 
 import csv
 import sqlite3
-import os 
+import os
+
+from datetime import date
+from datetime import datetime
 
 root = Tk()
 
 root.title('Address Book')
 
 root.geometry("600x600")
+
 
 #Statement to quickly print our queries (ex: query='''SELECT * From Customer''')
 def printQuery(query):
@@ -43,6 +49,126 @@ def modifyReturned():
                    END''')
       cars_conn.commit()
       cars_conn.close()
+
+
+
+
+
+
+# For tabs
+notebook = ttk.Notebook(root)
+notebook.grid()
+
+add_customer = Frame(notebook,width=600,height=600)
+add_vehicle = Frame(notebook,width=600,height=600)
+add_rental = Frame(notebook,width=600,height=600)
+
+add_customer.pack(fill="both",expand=1)
+add_vehicle.pack(fill="both",expand=1)
+add_rental.pack(fill="both",expand=1)
+
+notebook.add(add_customer,text="ADD Customer")
+notebook.add(add_vehicle,text="ADD Vehicle")
+notebook.add(add_rental,text="ADD Rental")
+
+
+
+#Query3
+def available_cars():
+      ac_conn = sqlite3.connect('cars.db')
+      ac_cur = ac_conn.cursor()
+      ac_cur.execute( """SELECT Distinct Vehicle.VehicleID, Description, 
+      Vehicle.Year,Vehicle.Type,Vehicle.Category 
+      FROM Vehicle,Rental WHERE VEHICLE.Type = ? AND 
+      Vehicle.Category = ? AND (( ? > Rental.ReturnDate) 
+      OR (? < Rental.StartDate));""",(vehicle_type.get(),vehicle_category.get(),
+      start_date.get(),end_date.get(),))
+      
+      query = ac_cur.fetchall()
+
+      for rental in query:
+            car_list.insert(0,rental)
+
+     
+      ac_conn.commit()
+      ac_conn.close()
+
+def make_rental():
+      row = car_list.get(car_list.curselection())
+
+      start = datetime.strptime(start_date.get(), "%Y-%m-%d").date()
+      end = datetime.strptime(end_date.get(), "%Y-%m-%d").date()
+      days =  (end - start).days
+
+      
+      today_date = datetime.today().strftime('%Y-%m-%d')
+      
+      rental_type = weekly.get()
+      if rental_type == 1:
+            if days % 7 != 0:
+                  rental_type = 1
+            else:
+                  rental_type = 7
+                  days = days/7
+                  
+      else:
+            rental_type = 1
+
+      mr_conn = sqlite3.connect('cars.db')
+      mr_cur = mr_conn.cursor()
+
+      mr_cur.execute("""SELECT Weekly,Daily FROM RATE WHERE TYPE = ? AND CATEGORY = ?""",(row[3],row[4]))
+      rate_amount = mr_cur.fetchall()
+   
+      
+      if rental_type == 7:
+            amount = rate_amount[0][0]*days
+      else:
+            amount = rate_amount[0][1]*days
+      payment_date = str(today_date) if which_payment.get() else end_date.get()
+      mr_cur.execute(""" INSERT INTO Rental(CustID,VehicleID,StartDate,OrderDate,
+      RentalType,Qty,ReturnDate,TotalAmount,PaymentDate,Returned) VALUES (?,?,?,?,?,?,?,?,?,?);""",(int(cust_id.get()),row[0],start_date.get(),
+      today_date,rental_type,days,end_date.get(),amount,payment_date,0))
+
+      mr_conn.commit()
+      mr_conn.close()
+
+
+vehicle_type = Entry(add_rental,width=1)
+vehicle_type.grid(row = 0, column = 1,)
+type_label = Label(add_rental, text = 'Type:').grid(row =0, column = 0)
+vehicle_category = Entry(add_rental,width=1)
+vehicle_category.grid(row = 0, column = 3)
+category_label = Label(add_rental, text = 'Category:').grid(row =0, column = 2)
+start_date = Entry(add_rental,width=10)
+start_date.grid(row = 1, column = 1)
+start_label = Label(add_rental, text = 'Start Date:').grid(row =1, column = 0)
+end_date = Entry(add_rental,width=10)
+end_date.grid(row = 1, column = 3)
+end_label = Label(add_rental, text = 'Return Date Date:').grid(row =1, column = 2)
+format =  Label(add_rental, text = 'Format: year-mm-dd').grid(row =1, column = 4)
+Search = Button(add_rental,text="Search",command=available_cars).grid(row =2, column = 0)
+car_list = Listbox(add_rental,width=50)
+car_list.grid(row =3,column=0)
+
+which_payment = IntVar()
+weekly = IntVar()
+
+check_pay = Checkbutton(add_rental,text = "Pay Now?",variable=which_payment)
+check_pay.grid(row=4,column=0)
+check_weekly= Checkbutton(add_rental,text = "Weekly Rate?",variable=weekly)
+check_weekly.grid(row=4,column=1)
+
+cust_id = Entry(add_rental,width=3)
+cust_id.grid(row = 5, column = 1)
+cust_label = Label(add_rental, text = 'CustID:').grid(row =5, column = 0)
+
+Submit =  Button(add_rental,text="Submit",command=make_rental).grid(row =6, column = 0)
+
+# END of query 3
+
+
+
 # #CREATE TABLES STATEMENTS
 # cars_conn = sqlite3.connect('cars.db')
 # c = cars_conn.cursor()
@@ -113,7 +239,4 @@ def modifyReturned():
 # modifyReturned()
 # printQuery('''SELECT * FROM Rental''')
 
-
-
-#executes tinker components
 root.mainloop()
